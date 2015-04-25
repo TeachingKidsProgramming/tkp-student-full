98e342526ca7bb5b90ac198118a95aebeea50328
Jim Counts <jamesrcounts@outlook.com>
2015-04-24 19:59:25 -0700

    Student Files

---
+++ src/org/teachingkidsprogramming/section04mastery/DigiFlower.java
@@ -0,0 +1,40 @@
+package org.teachingkidsprogramming.section04mastery;
+
+public class DigiFlower
+{
+  public static void main(String[] args)
+  {
+    //    Show the tortoise --#1
+    //    Make the tortoise move as fast as possible --#7
+    //    Make the background silver (HINT: Use PenColors) --#8
+    //    Make the line the tortoise draws 3 pixels wide --#20
+    //    createColorPalette (recipe below) --#9.1
+    //        Do the following 15 times --#19.1
+    //          drawOctogon (recipe below) --#10.1
+    //          Turn the tortoise 1/15th of 360 degrees to the right --#18
+    //        Repeat --#19.2
+    //    ------------- Recipe for createColorPalette --#9.2
+    //    Pen Color 1 is red --#3
+    //    Pen Color 2 is dark orange --#11
+    //    Pen Color 3 is gold --#12
+    //    Pen Color 4 is yellow --#13
+    //
+    //    Add color 1 to the color wheel --#3.1
+    //    Add color 2 to the color wheel --#11.1
+    //    Add color 3 to the color wheel --#12.1
+    //    Add color 4 to the color wheel --#13.1
+    //    Add color 4 to the color wheel --#14
+    //    Add color 3 to the color wheel --#15
+    //    Add color 2 to the color wheel --#16
+    //    Add color 1 to the color wheel --#17
+    //    ------------- End of createColorPalette recipe --#9.3
+    //
+    //    ------------- Recipe for drawOctogon --#10.2
+    //    Do the following 8 times --#6.1
+    //      Change the pen color of the line the tortoise draws to the next color on the color wheel --#4
+    //      Move the tortoise 50 pixels --#2
+    //      Turn the tortoise 1/8th of 360 degrees to the right --#5
+    //    Repeat --#6.2 
+    //    ------------- End of drawOctogon recipe --#10.3
+  }
+}
---
+++ src/org/teachingkidsprogramming/section05recursion/SpiderWeb.java
@@ -0,0 +1,35 @@
+package org.teachingkidsprogramming.section05recursion;
+
+public class SpiderWeb
+{
+  public static void main(String[] args)
+  {
+    //    Make the tortoise go as fast as possible --#6
+    //    Change the width of the line the tortoise draws to 1 pixel --#12
+    //    Change the Tortoise to a Spider --#14
+    //    Change the pen color of the line the tortoise draws to silver --#13
+    //    The current length of a line is 10.5 pixels --#1.2                  
+    //    The current zoom is 1.1 --#8.2
+    //    Do the following 10 times --#10
+    //        weaveOneLayer (recipe below) --#9.1
+    //
+    //        ------------- Recipe for weaveOneLayer --#9.2
+    //        Do the following 6 times --#5.1
+    //            drawTriangle (recipe below) --#4.1
+    //
+    //            ------------- Recipe for drawTriangle --#4.2
+    //            Do the following 3 times --#3
+    //                Move the tortoise the current length (of the line) --#1.1
+    //                Turn the tortoise 1/3rd of 360 degrees --#2
+    //            Repeat
+    //            ------------- End of drawTriangle recipe --#4.3
+    //
+    //            Turn the tortoise 1/6th of 360 degrees to the right --#7
+    //            Increase the current length (of the line) by the current zoom --#8.1
+    //        Repeat --#5.2
+    //        ------------- End of weaveOneLayer recipe --#9.3
+    //
+    //        Change the current zoom so it is multiplied by 1.3 --#11
+    //    Repeat
+  }
+}
